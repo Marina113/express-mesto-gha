19e4c68a03a8bbc23fd717258986d21bf8dac2a6
Marina113 <Marina-VL-113@yandex.ru>
2023-06-18 15:31:00 +0300

    бэк + фронт9

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const userRouter = require('./users');
 const cardRouter = require('./cards');
-const { ERROR_CODE } = require('../utils/constants');
+const { NOT_FOUND } = require('../utils/constants');
 
 router.use(userRouter);
 router.use(cardRouter);
 router.use((req, res) => {
-  res.status(ERROR_CODE).send({ message: 'Произошла ошибка' });
+  res.status(NOT_FOUND).send({ message: 'Произошла ошибка' });
 });
 
 module.exports = router;
